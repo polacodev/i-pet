dbd6e26cc22167219c3dc68af344667e6d1915c2
polacodev <polacodev@gmail.com>
2024-09-08 17:07:30 +0400

    chore: add async storage methods

--- utilities/async-storage.ts
+++ utilities/async-storage.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const storeData = async (key: string, value: string) => {
   try {
-    await AsyncStorage.setItem('key', value);
+    await AsyncStorage.setItem(key, value);
   } catch (error) {
     return error;
   }
@@ -16,5 +16,23 @@ export const getData = async (key: string) => {
     }
   } catch (error) {
     return error;
+  }
+};
+
+export const getAllKeys = async () => {
+  try {
+    return await AsyncStorage.getAllKeys();
+  } catch (e) {
+    // read key error
+    console.log(e);
+  }
+};
+
+export const removeValue = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {
+    // remove error
+    console.log(e);
   }
 };
